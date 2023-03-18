821d575fe7a7e3530f3e21a016d412227cc98357
WildboarG <959586@outlook.com>
2023-03-18 18:10:03 +0800

    用户信息后端接入前端

--- organ/main.go
+++ organ/main.go
@@ -3,7 +3,7 @@
  * @version: 1.0
  * @Date: 2023-03-09 17:41:59
  * @LastEditors: WildboarG
- * @LastEditTime: 2023-03-11 20:51:49
+ * @LastEditTime: 2023-03-18 18:05:51
  * @Descripttion:
  */
 package main
@@ -28,84 +28,256 @@ type Drom struct {
 	Name   string `json:"name"`
 	Status int    `json:"status"`
 }
+
 type UserInfo struct {
 	User     string `json:"user" form:"user"`
 	Password string `json:"password" form:"password"`
 }
 
-func Open(c *gin.Context) {
-	var datas Root
-	body, err := c.GetRawData()
-	// fmt.Println(body)
-	if err != nil {
-		fmt.Println(err)
-	}
-	err = json.Unmarshal(body, &datas)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	fmt.Println(datas)
-
-	var cards string
-	// 获取卡号 读卡器型号 做一个中间件然后记录进入时间，入口读卡器卡号
-	// 从数据库中比对数据 返回车牌号，若不存在返回"No"
-	cards = Search_card(datas.User)
-	print(cards)
-	var stus int
-	if cards != "Null" {
-		stus = 1 // 通行
-	} else {
-		stus = 2 // 不通行
-	}
-	c.JSON(200, gin.H{"name": cards, "status": stus})
+type Response struct {
+	Code int    `json:"code"`
+	Res  any    `json:"res"`
+	Msg  string `json:"msg"`
 }
+
+type Res struct {
+	Code  int `json:"code"`
+	Total int `json:"total"`
+	Users any `json:"users"`
+}
+
+type Adata struct { //数据库中的数据类型
+	Id   int    `json:"id" form:"id"`
+	Name string `json:"name" form:"name"`
+	Sex  int    `json:"sex" form:"sex"`
+	Age  int    `json:"age" form:"age"`
+	Idr  string `json:"idr" form:"idr"`
+	Car  string `json:"car" form:"car"`
+}
+
+type Query struct {
+	Page int    `json:"page" form:"page"`
+	Idr  string `json:"idr" form:"idr"`
+}
+
+type Delquery struct {
+	Idr string `json:"idr" form:"idr"`
+}
+
+var db_host string = "leveldb_test"
+
+// func Open(c *gin.Context) {
+// 	var datas Root
+// 	body, err := c.GetRawData()
+// 	// fmt.Println(body)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	err = json.Unmarshal(body, &datas)
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 	}
+// 	fmt.Println(datas)
+// 	var cards interface{}
+// 	// 获取卡号 读卡器型号 做一个中间件然后记录进入时间，入口读卡器卡号
+// 	// 从数据库中比对数据 返回车牌号，若不存在返回"No"
+// 	cards = Search_card(c, datas.User)
+// 	var dat map[string]interface{}
+// 	err = json.Unmarshal(string(cards), &dat)
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 	}
+// 	var stus int
+// 	if dat["err"] != nil {
+// 		stus = 1 // 通行
+// 	} else {
+// 		stus = 2 // 不通行
+// 	}
+// 	c.JSON(200, gin.H{"name": cards, "status": stus})
+// }
 
 func Login(c *gin.Context) {
 	if c.Request.Method == "GET" {
 		c.HTML(200, "login.html", nil)
+		// c.JSON(200, gin.H{"message": "登陆成功"})
 	}
 }
 
+// 登陆接口
 func Login_in(c *gin.Context) {
-	// fmt.Println(c.PostForm("user"))
-	// fmt.Println(c.PostForm("password"))
 	var userinfo UserInfo
+
 	err := c.ShouldBind(&userinfo)
-	fmt.Println(userinfo.User)
+	// fmt.Println(userinfo.User)
+	// fmt.Println(userinfo.Password)
+	if err != nil {
+		fmt.Println("[用户登陆]: " + "\033[31m" + err.Error() + "\033[0m")
+		return
+	}
+	if userinfo.User == "WildboarG" && userinfo.Password == "959586" {
+		fmt.Println("[用户登陆]: " + "\033[32m" + "登陆成功" + "\033[0m")
+		c.JSON(200, Response{200, "0", "登陆成功"})
+	} else {
+		fmt.Println("[用户登陆]: " + "\033[31m" + "账号或密码错误" + "\033[0m")
+		c.JSON(201, Response{201, "-1", "账号或密码错误"})
+	}
+}
+
+// 查询接口
+func Search_card(db_host string, c *gin.Context, card string) {
+	dat := util.Query_card(db_host, card)
+	// json.Marshal(dat)
+	c.JSON(200, Res{200, 1, dat})
+}
+
+// 查询所有
+// 想分组的 在这里修改
+func Search_all(db_host string, c *gin.Context) {
+	dat := util.Query_all(db_host)
+	//将切片转换为json
+	// fmt.Println(dat)
+	b, err := json.Marshal(dat)
+	if err != nil {
+		fmt.Println("[查询所有]: " + "\033[31m" + err.Error() + "\033[0m")
+	}
+	// fmt.Println(string(b))
+	var datak []Adata
+	if err := json.Unmarshal(b, &datak); err != nil {
+		// fmt.Println(err.Error())
+		fmt.Println("[查询所有]: " + "\033[31m" + "失败" + "\033[0m")
+	} else {
+		c.JSON(200,
+			Res{200, len(datak),
+				datak,
+			}) //返回json数据
+	}
+}
+
+// 查询选择
+func Search_choose(c *gin.Context) {
+	var querys Query
+
+	//绑定查询参数
+	err := c.ShouldBindQuery(&querys)
+
+	if err != nil {
+		c.JSON(200, gin.H{"mes": "参数错误"})
+		fmt.Println("[查询用户]: " + "\033[31m" + "绑定参数错误" + "\033[0m")
+		return
+	}
+
+	if querys.Idr == "" { //查询所有
+		Search_all(db_host, c)
+		fmt.Println("[查询所有]: " + "\033[32m" + "成功" + "\033[0m")
+	} else {
+		Search_card(db_host,
+			c,
+			string(querys.Idr),
+		)
+		fmt.Println("[查询用户]: " + "\033[32m" + querys.Idr + "\033[0m")
+	}
+}
+
+// 添加用户
+func Useradd(c *gin.Context) {
+	var data Adata
+	err := c.ShouldBind(&data)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-	if userinfo.User == "WildboarG" && userinfo.Password == "959586" {
-		c.HTML(200, "index.html", nil)
-	} else {
-		//c.HTML(200, "login.html", gin.H{"meg": "用户名或密码错误！"})
-		c.String(200, "么么密码")
+	// fmt.Println(data)
+	fmt.Println("[添加用户]: "+"\033[32m", "成功", "\033[0m")
+	util.Add_cards(db_host,
+		data.Id,
+		data.Name,
+		data.Sex,
+		data.Age,
+		data.Idr,
+		data.Car,
+	)
+	c.JSON(200, Response{200, "0", "添加成功"})
+}
+
+// 移除用户
+func Userremove(c *gin.Context) {
+	var data Delquery
+	err := c.ShouldBindQuery(&data)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
 	}
+	//终端彩色输出
+	fmt.Println("[删除用户]: "+"\033[32m", "成功", "\033[0m")
+	//fmt.Println(data)
+	util.Remove_cards(db_host, data.Idr)
+	c.JSON(200, Response{200, "0", "ok"})
 }
 
-func Search_card(card string) (name string) {
-	return util.Query_card("leveldb", card)
+// 修改用户
+func Userupdate(c *gin.Context) {
+	var data Adata
+	err := c.ShouldBindQuery(&data)
+	if err != nil {
+		fmt.Println("[修改用户]: "+"\033[31m", err.Error(), "\033[0m")
+		return
+	}
+	// fmt.Println(data)
+	fmt.Println("[修改用户]: "+"\033[32m", "成功", "\033[0m")
+	util.Update_cards(db_host,
+		data.Id,
+		data.Name,
+		data.Sex,
+		data.Age,
+		data.Idr,
+		data.Car,
+	)
+	c.JSON(200, Response{200, "0", "ok"})
 }
 
-// 遍历数据库
-func Search_all() {
-	util.Showdb("leveldb")
-}
-
-func Update(c *gin.Context) {
-	msg := map[string]string{"time": "18:18", "cardNumber": "001", "name": "zhangsan"}
-	c.HTML(200, "index.html", msg)
-}
 func main() {
-	//Search_all()
-	router := gin.Default()
+
+	// router := gin.Default()
+	router := gin.New()
+	router.Use(gin.Recovery())
+	router.Use(Cors())
 	router.LoadHTMLGlob("templates/html/*")
 	router.StaticFS("/static", http.Dir("./static"))
-	router.GET("/", Login)
-	router.POST("/", Login_in)
-	router.POST("/index", Update)
-	router.POST("/open", Open)
+	//路由分组
+	// v1 := router.Group("/v1")
+	// {
+	// 	v1.POST("/login", Login_in)
+	// 	v1.POST("/open", Open)
+	// }
+	user := router.Group("/user")
+	{
+		user.GET("/add", Useradd)
+		user.GET("/edit", Userupdate)
+		user.GET("/remove", Userremove)
+		user.GET("/listpage", Search_choose)
+	}
+	router.POST("/", Login_in) //登陆接口
+
+	// router.POST("/open", Open)
 
 	router.Run(":8080")
+}
+
+func Cors() gin.HandlerFunc { //解决跨域问题
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		c.Header("Access-Control-Allow-Origin", "*")                                                     // 可将将 * 替换为指定的域名
+		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization") //你想放行的header也可以在后面自行添加
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")              //我自己只使用 get post 所以只放行它
+		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+		c.Header("Access-Control-Allow-Credentials", "true")
+
+		// 放行所有OPTIONS方法
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+
+		// 处理请求
+		c.Next()
+	}
 }
--- organ/util/add_card.go
+++ organ/util/add_card.go
@@ -3,12 +3,15 @@
  * @version: 1.0
  * @Date: 2023-03-09 20:11:43
  * @LastEditors: WildboarG
- * @LastEditTime: 2023-03-11 11:01:36
+ * @LastEditTime: 2023-03-18 18:05:59
  * @Descripttion:
  */
 package util
 
+// package main
+
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/syndtr/goleveldb/leveldb"
@@ -23,10 +26,17 @@ type rms struct {
 	QUERY_OK    string
 	QUERY_NO    string
 	DELETE      string
+	UPDATE      string
 }
 
-// 实例化一个消息结构体
-var Mes rms = rms{"卡号错误", "卡号已注册", "注册成功", "查询成功", "查询失败", "删除成功"}
+var Mes rms = rms{"卡号错误",
+	"卡号已注册",
+	"注册成功",
+	"查询成功",
+	"查询失败",
+	"删除成功",
+	"更新成功",
+}
 
 func Add_card(db_host string, card string, name string) (mes string) {
 	db, err := leveldb.OpenFile(db_host, nil)
@@ -35,12 +45,13 @@ func Add_card(db_host string, card string, name string) (mes string) {
 	}
 	defer db.Close()
 	if len(card) != cardlen {
-		fmt.Println(Mes.ID_ERR)
+		// fmt.Println(Mes.ID_ERR)
+		fmt.Println("\033[31m" + Mes.ID_ERR + "\033[0m")
 		return Mes.ID_ERR
 	}
 	ret, _ := db.Has([]byte(card), nil)
 	if ret {
-		fmt.Println(Mes.AL_REGISTER)
+		fmt.Println("\033[36m" + Mes.AL_REGISTER + "\033[0m")
 		return Mes.AL_REGISTER
 	}
 	err = db.Put([]byte(card), []byte(name), nil) // 写入数据库
@@ -48,33 +59,114 @@ func Add_card(db_host string, card string, name string) (mes string) {
 		panic(err)
 	} else {
 		data, _ := db.Get([]byte(card), nil)
-		fmt.Println(Mes.REGISTER + string(data))
+		fmt.Println("\033[32m" + Mes.REGISTER + string(data) + "\033[0m")
 		return Mes.REGISTER
 	}
-
 }
 
-// 查询卡片
-func Query_card(db_host string, card string) (data string) {
+// 添加卡片
+func Add_cards(db_host string, idd int, name string, sex int, age int, idr string, car string) (mes string) {
 	db, err := leveldb.OpenFile(db_host, nil)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
-	if len(card) != cardlen {
-		fmt.Println(Mes.ID_ERR)
-		return
+	if len(idr) != cardlen {
+		fmt.Println("[添加信息]: " + "\033[31m" + Mes.ID_ERR + "\033[0m")
+		return Mes.ID_ERR
 	}
-	ret, _ := db.Has([]byte(card), nil)
+	ret, _ := db.Has([]byte(idr), nil)
 	if ret {
-		data, _ := db.Get([]byte(card), nil)
-		fmt.Println(Mes.QUERY_OK + string(data))
-		return string(data)
-	} else {
-		fmt.Println(Mes.QUERY_NO)
-		return "Null"
+		fmt.Println("[添加信息]: " + "\033[36m" + Mes.AL_REGISTER + "\033[0m")
+		return Mes.AL_REGISTER
 	}
 
+	var data = map[string]interface{}{
+		"idd":  idd,
+		"name": name,
+		"sex":  sex,
+		"age":  age,
+		"idr":  idr,
+		"car":  car,
+	}
+	// fmt.Println(data)
+	dx, _ := json.Marshal(data)
+	err = db.Put([]byte(string(idr)), []byte(dx), nil) // 写入数据库
+	if err != nil {
+		panic(err)
+	} else {
+		data, _ := db.Get([]byte(idr), nil)
+		fmt.Println("[" + Mes.REGISTER + "]" + "\033[32m" + string(data) + "\033[0m")
+		return Mes.REGISTER
+	}
+}
+
+// 更新卡片
+func Update_cards(db_host string, idd int, name string, sex int, age int, idr string, car string) (mes string) {
+	db, err := leveldb.OpenFile(db_host, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	if len(idr) != cardlen {
+		fmt.Println("[变更信息]:" + "\033[31m" + Mes.ID_ERR + "\033[0m")
+		return Mes.ID_ERR
+	}
+	ret, _ := db.Has([]byte(idr), nil)
+	if !ret {
+		fmt.Println("[变更信息]: " + "\033[36m" + Mes.ID_ERR + "\033[0m")
+		return Mes.ID_ERR
+	}
+	// var  dt []byte
+	// var dt = fmt.Sprintf("{\"idd\":\"%d\", \"name\":\"%s\", \"sex\":\"%d\", \"age\":\"%d\",  \"idr\":\"%s\",  \"car\":\"%s\"}", idd, name, sex, age, idr, car)
+	var content = map[string]interface{}{
+		"idd":  idd,
+		"name": name,
+		"sex":  sex,
+		"age":  age,
+		"idr":  idr,
+		"car":  car,
+	}
+
+	content_data, _ := json.Marshal(content)
+	err = db.Put([]byte(string(idr)), []byte(content_data), nil) // 写入数据库
+	if err != nil {
+		panic(err)
+	} else {
+		data, _ := db.Get([]byte(idr), nil)
+		// fmt.Println(Mes.UPDATE + string(data))
+		fmt.Println("[变更信息]: " + "\033[32m" + string(data) + "\033[0m")
+		return Mes.UPDATE
+	}
+}
+
+// 查询卡片
+func Query_card(db_host string, idr string) []interface{} {
+	db, err := leveldb.OpenFile(db_host, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	// if len(idr) != cardlen {
+	// 	fmt.Println(Mes.ID_ERR)
+	// 	return
+	// }
+	ret, _ := db.Has([]byte(idr), nil) // 判断是否存在
+	if ret {
+		var dat []interface{}
+		var dt interface{}
+		data, _ := db.Get([]byte(idr), nil)
+		json.Unmarshal(data, &dt) // 将value转换为json格式
+		dat = append(dat, dt)     // 将json格式的value添加到list中
+		// fmt.Println(Mes.QUERY_OK + string(data))
+		fmt.Println("[查询信息]: " + "\033[32m" + string(data) + "\033[0m")
+		return dat
+	} else {
+		// fmt.Println(Mes.QUERY_NO)
+		fmt.Println("[查询信息]: " + "\033[31m" + Mes.QUERY_NO + "\033[0m")
+		a := map[string]string{"err": "查询失败"}
+		return append([]interface{}{}, a)
+	}
 }
 
 // 删除卡片
@@ -84,45 +176,82 @@ func Delete_card(db_host string, card string) (mes string) {
 		panic(err)
 	}
 	defer db.Close()
-	if len(card) != cardlen {
-		fmt.Println(Mes.ID_ERR)
-		return Mes.ID_ERR
-	}
-	ret, _ := db.Has([]byte(card), nil)
-	if ret {
+
+	res, _ := db.Has([]byte(card), nil)
+	if res {
 		err := db.Delete([]byte(card), nil)
 		if err != nil {
 			panic(err)
 		} else {
-			fmt.Println(Mes.DELETE)
+			// fmt.Println(Mes.DELETE)
+			fmt.Println("[删除信息]: " + "\033[32m" + Mes.DELETE + "\033[0m")
 			return Mes.DELETE
 		}
 	} else {
-		fmt.Println(Mes.ID_ERR)
+		// fmt.Println(Mes.ID_ERR)
+		fmt.Println("[删除信息]: " + "\033[31m" + Mes.ID_ERR + "\033[0m")
 		return Mes.ID_ERR
 	}
 }
 
-// 遍历数据库
-func Showdb(db_host string) {
+// 删除卡片
+func Remove_cards(db_host string, idr string) (mes string) {
 	db, err := leveldb.OpenFile(db_host, nil)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
+
+	res, _ := db.Has([]byte(idr), nil)
+	// fmt.Println(res)
+	if res {
+		err := db.Delete([]byte(idr), nil)
+		if err != nil {
+			panic(err)
+		} else {
+			//fmt.Println(Mes.DELETE)
+			fmt.Println("[删除信息]: " + "\033[32m" + Mes.DELETE + "\033[0m")
+			return Mes.DELETE
+		}
+	} else {
+		// fmt.Println(Mes.ID_ERR)
+		fmt.Println("[删除信息]: " + "\033[31m" + Mes.ID_ERR + "\033[0m")
+		return Mes.ID_ERR
+	}
+}
+
+// 遍历数据库
+func Query_all(db_host string) []interface{} {
+	db, err := leveldb.OpenFile(db_host, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	var dta []interface{}
+	var dt interface{}
 	iter := db.NewIterator(nil, nil) // 创建迭代器
 	for iter.Next() {                // 遍历数据库
-		key := iter.Key()
+		//key := iter.Key()
 		value := iter.Value()
-		fmt.Printf("key=%s, value=%s\n", key, value) // 打印key和value
+		json.Unmarshal(value, &dt)
+		//fmt.Printf("idr=%s, data=%v\n", key, value) // 打印key和value
+		dta = append(dta, dt) // 将value添加到切片中
 	}
+
 	iter.Release()     // 释放迭代器
 	err = iter.Error() // 获取迭代器错误
 	if err != nil {
 		panic(err)
 	}
+	return dta
 }
 
 // func main() {
-// 	Add_card("0xd218982f", "MM12123")
+// 	Add_cards("leveldb1", 1, "李斯", 1, 18, "0x12345178", "京A12345")
+// 	Update_card("leveldb1", 1, "李斯", 1, 18, "0x12345178", "京A12345")
+// 	Query_card("leveldb1", "0x12345178")
+// 	Remove_card("leveldb1", "0x12345178")
+// 	Query_all("leveldb1")
 // }
